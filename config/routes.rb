ec5dea6bb12d78a58da0c7bdb35da75944f8511d
Mateusz Zdanowicz <mateusz.zdanowicz@elpassion.pl>
2016-09-05 17:21:52 +0200

    Create Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  get 'home/index'
+
+  get 'black_jack/index'
+  get 'black_jack/new'
+  get 'black_jack/show'
+  get 'black_jack/hit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
